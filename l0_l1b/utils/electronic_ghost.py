56671fac8a1527071e756f956a7092f77728accb
bekahalbach <bekah.albach@gmail.com>
2026-05-20 16:02:35 -0700

    change ghost correction to include the three other panels not all four per panel. also add it instead of subtract.

--- l0_l1b/utils/electronic_ghost.py
+++ l0_l1b/utils/electronic_ghost.py
@@ -13,38 +13,60 @@ def electronic_panel_ghost_correction(
     each ghost is .0048 a fraction of the original signal.
 
     From CRISM DPSIS (640 wide det):
-    DN'(x,y) =  DN(x,y)
-                - GHOST_DN(x,y)
-                - GHOST_DN(x+160, y)
-                - GHOST_DN(x+320, y)
-                - GHOST_DN(x+480, y)
-    So they subtracted the fractional ghost amount from every pixel using all
-    corresponding panel location pixels.
+       DN'(x,y) =  DN(x,y)
+                   - GHOST_DN(x,y)
+                   - GHOST_DN(x+160, y)
+                   - GHOST_DN(x+320, y)
+                   - GHOST_DN(x+480, y)
+    So they subtracted the fractional ghost amount from every pixel using a
+    all corresponding panel location pixels.
 
+    But for M3 the ghosts are actually depressions in signal, so we should
+    ADD the ghost offset. And I don't want to include the panel itself.
+    So more like:
+       DN'(x,y) =  DN(x,y)
+                   + GHOST_DN(x+160, y)
+                   + GHOST_DN(x+320, y)
+                   + GHOST_DN(x+480, y)
     In the list of corrections in the DPSIS this comes after interpolation
-    of 'bad' elements, so right now the ghosts of those are not
+    of 'bad' elements, so right now the ghosts of those are not accounted
+    for. It is not even clear to me if they occur?
     """
     if dark_cols is None:
         dark_cols = []
 
     panel_width = l0_samples // 4  # 80 for 320 etc
 
-    obs_data = obs_data.astype(np.float32)
     frame = obs_data.copy()
 
-    # don't use dark vals for correction bc they aren't dark subtracted
+    # don't use dark vals if we pass dark cols
+    # for correction bc they aren't dark subtracted
     # this is a bekah design decision we could change
     frame[:, :, dark_cols] = 0
 
-    # sum the 4 panels
-    frame_panels = frame.reshape(obs_data.shape[0], obs_data.shape[1], 4,
-                                 panel_width)
-    ghost_signal = ghost_correction * frame_panels.sum(axis=2)
+    # split by 4 panels 80 or 160 wide
+    frame_panels = frame.reshape(
+        obs_data.shape[0],
+        obs_data.shape[1],
+        4,
+        panel_width
+    )
+    obs_data_reshaped = obs_data.reshape(
+        obs_data.shape[0], obs_data.shape[1],
+        4,
+        panel_width
+    )
+    # sum other 3 panels and multiply by correction factor
+    for panel_idx in range(4):
+        other_panels = np.concatenate(
+            [frame_panels[:, :, :panel_idx, :],
+             frame_panels[:, :, panel_idx+1:, :]],
+            axis=2
+        )
+        ghost_signal = ghost_correction * other_panels.sum(axis=2)
+        obs_data_reshaped[:, :, panel_idx, :] += ghost_signal
 
-    # reshape obs data for subtraction
-    obs_data = obs_data.reshape(obs_data.shape[0], obs_data.shape[1], 4,
-                                panel_width)
-
-    obs_data -= ghost_signal[:, :, np.newaxis, :]
-
-    return obs_data.reshape(obs_data.shape[0], obs_data.shape[1], -1)
+    return obs_data_reshaped.reshape(
+        obs_data_reshaped.shape[0],
+        obs_data_reshaped.shape[1], -1
+    )
